1be89b416e895228eef0dd3c9fe5b4c949504fb6
Dirk Callaway <35826733+dirkcallaway@users.noreply.github.com>
2019-03-02 15:50:20 -0700

    Initial file structure upload

---
+++ config/connection.js
@@ -0,0 +1,34 @@
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
+
+// Require mysql
+var mysql = require("mysql");
+
+// Set up our connection information
+//mySQL connection info
+var connection = mysql.createConnection({
+  host: process.env.DB_host,
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: process.env.DB_USER,
+
+  // Your password
+  password: process.env.DB_PASS,
+  database: process.env.DB
+});
+
+// Connect to the database
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT burgers (burger_name)
+VALUES ("Impossible Burger");
+
+INSERT burgers (burger_name)
+VALUES ("Veggie Burger");
+
+INSERT burgers (burger_name)
+VALUES ("Beyond Meat Burger");
